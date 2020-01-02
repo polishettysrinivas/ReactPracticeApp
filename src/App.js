43c4dc940e6934b45a5dafaf1eb0fbd86fab55d1
Srini <srinivaspolishetty@gmail.com>
2020-01-02 21:05:04 +0530

    react practice app
    react practice app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 1, name: 'Srinivas Polishetty', age: 33 },
+      { id: 2,  name: 'Swetha Rani', age: 29 },
+      { id: 3,  name: 'Sreeyansh Polishetty', age: 5 }
+    ],
+    showPersons: false
+  }
+
+  togglePersons = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+
+  }
+
+ deletePersonHandler = (personIndex) => {
+   const persons = [...this.state.persons];
+   persons.splice(personIndex, 1);
+   this.setState({persons : persons})
+ }
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      border: '1px solid blue',
+      padding: '16px',
+      cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person key={person.id}
+            click={() => this.deletePersonHandler(index)} 
+            name={person.name} 
+            age={person.age} />
+          })}
+        </div>
+      )
+    }
+    return (
+      <div className="App">
+        <h1>Hi! I am a React App</h1>
+        <p>This is really working</p>
+        <button
+          style={style}
+          onClick={this.togglePersons}>Toggle Persons
+         </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
